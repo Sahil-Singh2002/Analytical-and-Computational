9db703428589ce85fde3203c66b9b1a9f0a1218a
Sahil Singh <125428501+Sahil-Singh2002@users.noreply.github.com>
2023-06-10 23:51:03 +0100

    Analytical_and_Computational_Foundation.py

--- Analytical_and_Computational_Foundation_Coursework_1.py
+++ Analytical_and_Computational_Foundation_Coursework_1.py
@@ -1,33 +1,29 @@
-
-def babylonian_square_root(a,e,x):
-    assert a>0
-    assert e>0
-    N=0
-    x= abs(0.5*(x+(a/x)))
-    while abs((x**2)-a)>e:
-         x= 0.5*(x+(a/x))
-         N+=1
-    return (x,N)
-
-
-def babylonian_square_root_list(a,e,x):
-    # Your code here
-    x_list=[x]
-    assert a>0
-    assert e>0
-    while abs(((x_list[-1])**2)-a)>e:
-         x_list.append(0.5*((x_list[-1])+(a/(x_list[-1]))))
-    return x_list
-    
-
 import pylab
 import math
 
-xy= babylonian_square_root_list(20000,1e-10,1)
-xv= pylab.linspace(0,len(xy),len(xy))
-vu=[math.sqrt(20000) for _ in range(len(xv))]
-pylab.scatter(xv,xy,label='$x_n$')
-pylab.plot(xv,vu,label="$\sqrt{20000}$",color="red")
+def babylonian_square_root(a, e, x):
+    assert a > 0
+    assert e > 0
+    N = 0
+    x = abs(0.5 * (x + (a / x)))
+    while abs(x * x - a) > e * a:
+        x = 0.5 * (x + (a / x))
+        N += 1
+    return x, N
+
+def babylonian_square_root_list(a, e, x):
+    assert a > 0
+    assert e > 0
+    x_list = [x]
+    while abs(x_list[-1] * x_list[-1] - a) > e * a:
+        x_list.append(0.5 * (x_list[-1] + (a / x_list[-1])))
+    return x_list
+
+xy = babylonian_square_root_list(20000, 1e-10, 1)
+xv = pylab.linspace(0, len(xy), len(xy))
+vu = [math.sqrt(20000) for _ in range(len(xv))]
+pylab.scatter(xv, xy, label='$x_n$')
+pylab.plot(xv, vu, label="$\sqrt{20000}$", color="red")
 
 pylab.legend(loc='lower left')
 pylab.xlabel('$n$')
